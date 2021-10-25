9e3a395fc78467d5bdd7c2900ba25828706d9387
Michał Bruchwalski <mbruchwalski@gmail.com>
2021-10-25 10:48:08 +0200

    Add project files.

---
+++ FileDownloader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
